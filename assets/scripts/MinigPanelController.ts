d1cfb35053b2ef70366757641fd0d6f7b8b0800f
Vergusha <Vergillyi@outlook.com>
2025-05-15 20:07:01 +0200

    Корректно работающий префаб массива [#8]

--- assets/scripts/MinigPanelController.ts
+++ assets/scripts/MinigPanelController.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Label, Node } from 'cc';
+import { _decorator, Component, Label, Node, Prefab, instantiate } from 'cc';
 import { GamePanelController } from './GamePanelController';
 const { ccclass, property } = _decorator;
 
@@ -8,10 +8,6 @@ type Upgrade = {
     baseCost: number;
     cost: number;
     effect: number;
-    nameLabel?: Label;
-    levelLabel?: Label;
-    costLabel?: Label;
-    buyButton?: Node;
 };
 
 @ccclass('MiningPanelController')
@@ -19,17 +15,11 @@ export class MiningPanelController extends Component {
     @property(GamePanelController)
     gamePanelController: GamePanelController = null;
 
-    @property([Label])
-    upgradeNameLabels: Label[] = [];
+    @property(Prefab)
+    upgradeItemPrefab: Prefab = null;
 
-    @property([Label])
-    upgradeLevelLabels: Label[] = [];
-
-    @property([Label])
-    upgradeCostLabels: Label[] = [];
-
-    @property([Node])
-    upgradeBuyButtons: Node[] = [];
+    @property(Node)
+    upgradesContainer: Node = null; // Контейнер для апгрейдов (например, Vertical Layout)
 
     private upgrades: Upgrade[] = [
         { name: 'Бур', level: 1, baseCost: 10, cost: 10, effect: 1 },
@@ -38,38 +28,39 @@ export class MiningPanelController extends Component {
     ];
 
     start() {
-        // Привязываем лейблы и кнопки к апгрейдам
         for (let i = 0; i < this.upgrades.length; i++) {
-            this.upgrades[i].nameLabel = this.upgradeNameLabels[i];
-            this.upgrades[i].levelLabel = this.upgradeLevelLabels[i];
-            this.upgrades[i].costLabel = this.upgradeCostLabels[i];
-            this.upgrades[i].buyButton = this.upgradeBuyButtons[i];
-            // Вешаем обработчик на каждую кнопку
-            if (this.upgrades[i].buyButton) {
-                this.upgrades[i].buyButton.on(Node.EventType.TOUCH_END, () => this.buyUpgrade(i), this);
+            const upg = this.upgrades[i];
+            const item = instantiate(this.upgradeItemPrefab);
+            this.upgradesContainer.addChild(item);
+
+            // Получаем компоненты внутри префаба
+            const nameLabel = item.getChildByName('NameLabel')?.getComponent(Label);
+            const levelLabel = item.getChildByName('LevelLabel')?.getComponent(Label);
+            const costLabel = item.getChildByName('CostLabel')?.getComponent(Label);
+            const buyButton = item.getChildByName('BuyButton');
+
+            if (nameLabel) nameLabel.string = upg.name;
+            if (levelLabel) levelLabel.string = `Ур. ${upg.level}`;
+            if (costLabel) costLabel.string = `${upg.cost}`;
+
+            if (buyButton) {
+                buyButton.on(Node.EventType.TOUCH_END, () => {
+                    this.buyUpgrade(i, levelLabel, costLabel);
+                }, this);
             }
         }
-        this.updateAllLabels();
     }
 
-    buyUpgrade(index: number) {
+    buyUpgrade(index: number, levelLabel: Label, costLabel: Label) {
         const upgrade = this.upgrades[index];
         if (this.gamePanelController['dilitium'] >= upgrade.cost) {
             this.gamePanelController['dilitium'] -= upgrade.cost;
             upgrade.level += 1;
             upgrade.cost = Math.floor(upgrade.baseCost * Math.pow(1.5, upgrade.level - 1));
             this.gamePanelController['dilitiumPerClick'] += upgrade.effect;
-            this.updateAllLabels();
+            if (levelLabel) levelLabel.string = `Ур. ${upgrade.level}`;
+            if (costLabel) costLabel.string = `${upgrade.cost}`;
             this.gamePanelController.updateDilitiumText();
-        }
-    }
-
-    updateAllLabels() {
-        for (let i = 0; i < this.upgrades.length; i++) {
-            const upg = this.upgrades[i];
-            if (upg.nameLabel) upg.nameLabel.string = upg.name;
-            if (upg.levelLabel) upg.levelLabel.string = `Ур. ${upg.level}`;
-            if (upg.costLabel) upg.costLabel.string = `${upg.cost}`;
         }
     }
 }
