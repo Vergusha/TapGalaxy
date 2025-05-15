4b34245a985f139c1c3c60aa17896de6c3fe0b9a
Vergusha <Vergillyi@outlook.com>
2025-05-15 19:47:01 +0200

    Добавлен первый массив [#7]

--- assets/scripts/MinigPanelController.ts
+++ assets/scripts/MinigPanelController.ts
@@ -1,39 +1,75 @@
-import { _decorator, Component, Node, Label, Button } from 'cc';
+import { _decorator, Component, Label, Node } from 'cc';
 import { GamePanelController } from './GamePanelController';
 const { ccclass, property } = _decorator;
+
+type Upgrade = {
+    name: string;
+    level: number;
+    baseCost: number;
+    cost: number;
+    effect: number;
+    nameLabel?: Label;
+    levelLabel?: Label;
+    costLabel?: Label;
+    buyButton?: Node;
+};
 
 @ccclass('MiningPanelController')
 export class MiningPanelController extends Component {
     @property(GamePanelController)
     gamePanelController: GamePanelController = null;
 
-    @property(Label)
-    upgradeCostLabel: Label = null;
+    @property([Label])
+    upgradeNameLabels: Label[] = [];
 
-    @property(Label)
-    levelLabel: Label = null;
+    @property([Label])
+    upgradeLevelLabels: Label[] = [];
 
-    private upgradeCost: number = 10;
-    private level: number = 1;
+    @property([Label])
+    upgradeCostLabels: Label[] = [];
+
+    @property([Node])
+    upgradeBuyButtons: Node[] = [];
+
+    private upgrades: Upgrade[] = [
+        { name: 'Бур', level: 1, baseCost: 10, cost: 10, effect: 1 },
+        { name: 'Шахтер', level: 1, baseCost: 50, cost: 50, effect: 5 },
+        { name: 'Дрон', level: 1, baseCost: 200, cost: 200, effect: 20 },
+    ];
 
     start() {
-        this.updateLabels();
+        // Привязываем лейблы и кнопки к апгрейдам
+        for (let i = 0; i < this.upgrades.length; i++) {
+            this.upgrades[i].nameLabel = this.upgradeNameLabels[i];
+            this.upgrades[i].levelLabel = this.upgradeLevelLabels[i];
+            this.upgrades[i].costLabel = this.upgradeCostLabels[i];
+            this.upgrades[i].buyButton = this.upgradeBuyButtons[i];
+            // Вешаем обработчик на каждую кнопку
+            if (this.upgrades[i].buyButton) {
+                this.upgrades[i].buyButton.on(Node.EventType.TOUCH_END, () => this.buyUpgrade(i), this);
+            }
+        }
+        this.updateAllLabels();
     }
 
-    onUpgradeButtonClick() {
-        // Проверяем, хватает ли дилития
-        if (this.gamePanelController['dilitium'] >= this.upgradeCost) {
-            this.gamePanelController['dilitium'] -= this.upgradeCost;
-            this.gamePanelController.upgradeMining();
-            this.level += 1;
-            this.upgradeCost = Math.floor(this.upgradeCost * 1.5);
-            this.updateLabels();
+    buyUpgrade(index: number) {
+        const upgrade = this.upgrades[index];
+        if (this.gamePanelController['dilitium'] >= upgrade.cost) {
+            this.gamePanelController['dilitium'] -= upgrade.cost;
+            upgrade.level += 1;
+            upgrade.cost = Math.floor(upgrade.baseCost * Math.pow(1.5, upgrade.level - 1));
+            this.gamePanelController['dilitiumPerClick'] += upgrade.effect;
+            this.updateAllLabels();
             this.gamePanelController.updateDilitiumText();
         }
     }
 
-    updateLabels() {
-        if (this.upgradeCostLabel) this.upgradeCostLabel.string = this.upgradeCost.toString();
-        if (this.levelLabel) this.levelLabel.string = this.level.toString();
+    updateAllLabels() {
+        for (let i = 0; i < this.upgrades.length; i++) {
+            const upg = this.upgrades[i];
+            if (upg.nameLabel) upg.nameLabel.string = upg.name;
+            if (upg.levelLabel) upg.levelLabel.string = `Ур. ${upg.level}`;
+            if (upg.costLabel) upg.costLabel.string = `${upg.cost}`;
+        }
     }
 }
